af2e41fc531d94a1d4799e6c1edb1fc01143547f
Beesigaa <adejohadamah@gmial.com>
2024-11-10 16:42:10 +0000

    editing landingpage

--- smart_waste_management/settings.py
+++ smart_waste_management/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'smart_waste_management.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
